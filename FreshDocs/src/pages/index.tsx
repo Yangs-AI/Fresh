d2504797267f83ca15501c95c9404a5cc4528c19
Jason Young <AI.Jason.Young@outlook.com>
2025-10-22 14:40:26 +0800

    Logo & Style & Content (#2)
    * Init Fresh Docs
    
    * Update
    
    * Update
    
    * Update
    
    * Update
    
    * Update
    
    * Change Description
    
    * Clean & Add Jason Author
    
    * Clean & Add AIDAI & Fresh Tags...
    
    * Clean
    
    * Fix Bug
    
    * Begin: Contribute
    
    * Change Logo
    
    * Change CSS

--- FreshDocs/src/pages/index.tsx
+++ FreshDocs/src/pages/index.tsx
@@ -1,43 +1,19 @@
 import type {ReactNode} from 'react';
-import clsx from 'clsx';
-import Link from '@docusaurus/Link';
-import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
+
 import Layout from '@theme/Layout';
-import HomepageFeatures from '@site/src/components/HomepageFeatures';
-import Heading from '@theme/Heading';
 
-import styles from './index.module.css';
+import HomePageHeader from '@site/src/components/HomePage/Header';
+import HomePageFeatures from '@site/src/components/HomePage/Features';
 
-function HomepageHeader() {
-  const {siteConfig} = useDocusaurusContext();
-  return (
-    <header className={clsx('hero hero--primary', styles.heroBanner)}>
-      <div className="container">
-        <Heading as="h1" className="hero__title">
-          {siteConfig.title}
-        </Heading>
-        <p className="hero__subtitle">{siteConfig.tagline}</p>
-        <div className={styles.buttons}>
-          <Link
-            className="button button--secondary button--lg"
-            to="/docs/intro">
-            Docusaurus Tutorial - 5min ⏱️
-          </Link>
-        </div>
-      </div>
-    </header>
-  );
-}
 
 export default function Home(): ReactNode {
-  const {siteConfig} = useDocusaurusContext();
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
-      <HomepageHeader />
+      title={`Friendly Research Resources Hub`}
+      description="Making research resources easy to find, reuse, and share - documenting assets for reproducible research.">
+      <HomePageHeader />
       <main>
-        <HomepageFeatures />
+        <HomePageFeatures />
       </main>
     </Layout>
   );
