99f61e47b254a71f11e9d9f5e41d9b679b3699a9
Jason-Young-AI <AI.Jason.Young@outlook.com>
2025-10-25 22:31:02 +0800

    Update

--- FreshDocs/docusaurus.config.ts
+++ FreshDocs/docusaurus.config.ts
@@ -103,7 +103,7 @@ const config: Config = {
 
   themeConfig: {
     // Replace with your project's social card
-    image: 'img/docusaurus-social-card.jpg',
+    image: 'img/fresh-social-card.jpg',
     colorMode: {
       respectPrefersColorScheme: true,
     },
